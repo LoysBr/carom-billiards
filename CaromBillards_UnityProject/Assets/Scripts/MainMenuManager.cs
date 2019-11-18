c8f75cd4a8b59b9a293d0fdb79dbe59f64e05b8f
Loïs B <abeetlott@gmail.com>
2019-11-18 20:56:28 +0100

    + scenes MainMenu and SinglePlayer ; + MainMenuManager
    Just a simple menu with 1 button loading the game scene.

---
+++ CaromBillards_UnityProject/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public string m_SceneToLaunch;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void OnNewGameButtonClick()
+    {
+        SceneManager.LoadScene(m_SceneToLaunch);
+    }
+}
