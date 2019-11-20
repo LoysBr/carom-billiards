978bb49a2c6ccee7a867ddbfd7a5c04b9ec77033
Loïs B <abeetlott@gmail.com>
2019-11-20 22:25:22 +0100

    + Collision with other balls thx to Physic
    not working as I would like but it's a good start

--- CaromBilliards_UnityProject/Assets/Scripts/Ball.cs
+++ CaromBilliards_UnityProject/Assets/Scripts/Ball.cs
@@ -6,59 +6,34 @@ public class Ball : MonoBehaviour
 {
     [HideInInspector]
     public float        m_maxSpeed; //in m/s
-    [HideInInspector]
-    public float        m_friction; //also in m/s
 
-    private Vector3     m_direction; 
-
-    private float       m_currentSpeed;
+    private Rigidbody   m_rigidBody;
 
     void Start()
     {
-        m_currentSpeed = 0;
+        m_rigidBody = GetComponent<Rigidbody>();
     }   
 
+    //collision with other balls
     public void OnCollisionEnter(Collision collision)
     {
-        //Vector3 colliderDir = Vector3.Cross(collision.GetContact(0).normal, Vector3.up).normalized;
-        //Debug.Log("colliderDir : " + colliderDir);
-
-        //Vector3 colliderNorm = collision.GetContact(0).normal;
-        //Debug.Log("colliderNorm  : " + colliderNorm);
-       
-
-        //float collisionAngle = Vector3.Angle(colliderDir, m_direction);
-        //Debug.Log("angle : " + collisionAngle);
-
-        //Vector3 newDir = Mathf.Cos(collisionAngle) * colliderDir + Mathf.Sin(collisionAngle) * colliderNorm;
-
-        //Debug.Log("previousDir : " + m_direction);
-        //Debug.Log("newDir : " + newDir);
-
-        //m_direction = newDir;
+        
     }
 
     //collision with cushions
     public void OnTriggerEnter(Collider other)
     {
-        Vector3 newDir = m_direction - 2 * Vector3.Dot(m_direction, other.transform.forward) * other.transform.forward;
-        m_direction = newDir.normalized;
+        Vector3 newVelocity = m_rigidBody.velocity - 2 * Vector3.Dot(m_rigidBody.velocity, other.transform.forward) * other.transform.forward;
+        m_rigidBody.velocity = newVelocity;
     }
 
     void Update()
     {
-        //movement
-        if(m_currentSpeed > 0)
-        {
-            transform.position += m_direction * m_currentSpeed * Time.deltaTime;
-
-            m_currentSpeed -= m_friction * Time.deltaTime;
-        }
+        
     }
 
     public void OnBallShot(float _power, Vector3 _dir)
-    {
-        m_currentSpeed = m_maxSpeed * _power;
-        m_direction = _dir;
+    {               
+        m_rigidBody.velocity = m_maxSpeed * _power * _dir;
     }
 }
