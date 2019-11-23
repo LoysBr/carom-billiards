55211d75dd9c0fb481268ecfc2d60dedf706ffe8
Loïs B <abeetlott@gmail.com>
2019-11-23 19:59:46 +0100

    little refacto

--- CaromBilliards_UnityProject/Assets/Scripts/InputManager.cs
+++ CaromBilliards_UnityProject/Assets/Scripts/InputManager.cs
@@ -2,12 +2,11 @@
 
 public class InputManager : MonoBehaviour
 {
-    public delegate void InputShot(float _power);
-    public event InputShot InputShotEvent;
-    public delegate void InputShotHolded(float _currentPower);
-    public event InputShotHolded InputShotHoldEvent;
-    public delegate void InputCameraRotation(float _angle);
-    public event InputCameraRotation InputCameraRotationEvent;
+    public delegate void    InputShot(float _power);
+    public event            InputShot InputShotEvent;
+    public event            InputShot InputShotHoldEvent;
+    public delegate void    InputCameraRotation(float _angle);
+    public event            InputCameraRotation InputCameraRotationEvent;
 
     [HideInInspector]
     public float m_shotMaxHoldDuration;
